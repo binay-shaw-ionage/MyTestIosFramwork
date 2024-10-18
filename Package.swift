c4719ca356d1b8a586b0d114384f3f3a5706db1b
Binay Shaw <binay.s@ionage.in>
2024-10-18 14:19:58 +0530

    change in package code

--- Package.swift
+++ Package.swift
@@ -1,25 +1,3 @@
-//// swift-tools-version:5.3
-//import PackageDescription
-//
-//let package = Package(
-//    name: "MyTestIosFramwork",
-//    products: [
-//        .library(
-//            name: "MyTestIosFramwork",
-//            targets: ["MyTestIosFramwork"]),
-//    ],
-//    dependencies: [],
-//    targets: [
-//        .target(
-//            name: "MyTestIosFramwork",
-//            dependencies: []),
-//        .testTarget(
-//            name: "MyTestIosFramworkTests",
-//            dependencies: ["MyTestIosFramwork"]),
-//    ]
-//)
-
-
 // swift-tools-version:5.9
 import PackageDescription
 
